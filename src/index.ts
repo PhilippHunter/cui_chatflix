2a2275bb62a33791990dc2e9987c96c2156b209a
PhilippHunter <philippjaeger98@web.de>
2019-12-23 16:34:24 +0100

    checking for title availibility + intentRename

--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ app.intent('InformationIntent', (conv, params) => {
         conv.ask('Möchtest du noch mehr Informationen zu den Schauspielern erfahren?')
     }
     else {
-        conv.ask('Leider habe ich diesen Film nicht gefunden. Probiere es mit einem anderem.')
+        conv.ask('Leider habe ich den gewünschten Titel nicht gefunden. Probiere es mit einem anderen.')
     }
 });
 
@@ -34,10 +34,15 @@ app.intent('ActorInformation-FollowupIntent', (conv, params) => {
     let parameters = conv.contexts.get('informationintent-followup').parameters
     const input = parameters.Serie ? parameters.Serie : parameters.Film;
     console.log(input);
+    if(input !== ''){
+        let actors = dataHandler.getActors(input);
+        actors.forEach(s => resultString = resultString + '\n' + s);
+        conv.ask('Es spielen folgende Schauspieler mit: \n' +resultString);
+    }
+    else {
+        conv.ask('Leider habe den gewünschten Titel nicht gefunden. Probiere es mit einem anderen.')
+    }
 
-    let actors = dataHandler.getActors(input);
-    actors.forEach(s => resultString = resultString + '\n' + s);
-    conv.ask('Es spielen folgende Schauspieler mit: \n' +resultString);
 });
 
 app.intent('ActorInformationIntent', (conv, params) => {
@@ -52,20 +57,24 @@ app.intent('ActorInformationIntent', (conv, params) => {
         conv.ask('Es spielen folgende Schauspieler mit: \n' +resultString);
     }
     else {
-        conv.ask('Leider habe ich diesen Film nicht gefunden. Probiere es mit einem anderem.')
+        conv.ask('Leider habe ich den gewünschten Titel nicht gefunden. Probiere es mit einem anderen.')
     }
 });
 
 app.intent('GenreIntent', (conv, params) => {
-    let genre = params.Genre
-    let resultString: string = '';
+    let genre = params.Genre;
+    console.log(genre);
     let result = dataHandler.getMoviesInGenre(genre);
-    if(result.length != 0) {
-        console.log(result)
+    if(result.length !== 0) {
+        console.log(result);
         conv.ask('Hier sind ein paar Vorschläge:')
+        let resultString = '';
         result.forEach(s => resultString = resultString + '\n' + s);
+        conv.ask(resultString);
     }
-    conv.ask(resultString);
+    else {
+        conv.ask('Leider habe ich keinen Titel im gewünschten Genre gefunden. Probiere es mit einem anderen.');
+    }
 })
 
 app.intent('InspirationWatchlistIntent', (conv) => {
@@ -75,8 +84,11 @@ app.intent('InspirationWatchlistIntent', (conv) => {
         console.log(result)
         conv.ask('Hier sind ein paar Vorschläge:')
         result.forEach(s => resultString = resultString + '\n' + s);
+        conv.ask(resultString);
     }
-    conv.ask(resultString);
+    else {
+        conv.ask('Leider befinden sich momentan noch keine Filme in deiner Watchlist.');
+    }
 })
 
 app.intent('InspirationCurrentlyLikedIntent', (conv) => {
@@ -90,7 +102,7 @@ app.intent('InspirationCurrentlyLikedIntent', (conv) => {
     conv.ask(resultString);
 })
 
-app.intent('WatchlistIntent', (conv, params) => {
+app.intent('AppendWatchlistIntent', (conv, params) => {
     console.log(conv.intent);
     console.log(params);
     const input = params.Serie ? params.Serie : params.Film;
@@ -105,6 +117,23 @@ app.intent('WatchlistIntent', (conv, params) => {
     }
     else {
         conv.ask('Leider habe ich diesen Film nicht gefunden. Probiere es mit einem anderem.')
+    }
+})
+
+app.intent('ShowWatchlistIntent', (conv, params) => {
+    //CODE DUPLIKAT!
+
+    console.log(params);
+    let resultString: string = '';
+    let result = dataHandler.getMoviesInWatchlist();
+    if(result.length != 0) {
+        console.log(result)
+        conv.ask('Hier sind ein paar Vorschläge:')
+        result.forEach(s => resultString = resultString + '\n' + s);
+        conv.ask(resultString);
+    }
+    else {
+        conv.ask('Leider befinden sich momentan noch keine Filme in deiner Watchlist.');
     }
 })
 
